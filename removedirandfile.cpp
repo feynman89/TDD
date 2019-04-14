a5c9d2c3d9d8a3de6a880904be06a013c0dace99
Den <feynman89@users.noreply.github.com>
2019-04-14 17:53:05 +0700

    Рефакторим метод removeDir основного класса: собираем все, для полного удаления папки, в том числе рекурсивного

--- removedirandfile.cpp
+++ removedirandfile.cpp
@@ -15,6 +15,27 @@ bool RemoveDirAndFile::removeFile(QString link)
 
 bool RemoveDirAndFile::removeDir(QString link)
 {
+    //Получаем список каталогов
+    QStringList lstDirs = getAllFoldersInDir(link);
+
+    //Получаем список файлов
+    QStringList lstFiles = getAllFilesInDir(link);
+
+    //Удаляем файлы
+    foreach (QString entry, lstFiles)
+    {
+        QString entryAbsPath = link + "/" + entry;
+        QFile::setPermissions(entryAbsPath, QFile::ReadOwner | QFile::WriteOwner);
+        removeFile(entryAbsPath);
+    }
+
+    //Для папок делаем рекурсивный вызов
+    foreach (QString entry, lstDirs)
+    {
+        QString entryAbsPath = link + "/" + entry;
+        removeDir(entryAbsPath);
+    }
+
     if (QDir().rmdir(link))
         return  true;
     else
